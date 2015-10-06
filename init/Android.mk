RazerRom/system_core

init: force SELinux to be permissive on all builds
 - Less secure, yes.
 - But also, much stable ROM, too.

And realistically, every temasek ROM users use with root access.
Come on, there's no need to enforce SELinux here.

Signed-off-by: arter97 <qkrwngud825@gmail.com>

init/Android.mk
@@ -25,9 +25,7 @@ LOCAL_SRC_FILES += bootchart.c
 LOCAL_CFLAGS    += -DBOOTCHART=1
 endif
 
-ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
-LOCAL_CFLAGS += -DALLOW_LOCAL_PROP_OVERRIDE=1 -DALLOW_DISABLE_SELINUX=1
-endif
+LOCAL_CFLAGS += -DALLOW_LOCAL_PROP_OVERRIDE=1
 
 # Enable ueventd logging
 #LOCAL_CFLAGS += -DLOG_UEVENTS=1

DesolationStaging/marshsystemcore

init/Android.mk
@@ -4,11 +4,7 @@ LOCAL_PATH:= $(call my-dir)
 
 # --
 
-ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
-init_options += -DALLOW_LOCAL_PROP_OVERRIDE=1 -DALLOW_DISABLE_SELINUX=1
-else
-init_options += -DALLOW_LOCAL_PROP_OVERRIDE=0 -DALLOW_DISABLE_SELINUX=0
-endif
+init_options += -DALLOW_LOCAL_PROP_OVERRIDE=1
 
 init_options += -DLOG_UEVENTS=0
 